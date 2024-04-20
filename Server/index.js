fc83757d579a1b3559f28336b8c8218a26923ac3
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-04-20 15:12:28 +0530

    express1

--- Server/index.js
+++ Server/index.js
@@ -2,10 +2,12 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
-const server = http.createServer((req, res) => {
-  const log = `${Date.now()}: ${req.url} New request\n`;
+function myHandler(req, res) {
+  const log = `${Date.now()}: ${req.method}: ${req.url} New request\n`;
   const myUrl = url.parse(req.url, true);
-  if(myUrl.pathname === "/favicon.ico"){return res.end()};
+  if (myUrl.pathname === "/favicon.ico") {
+    return res.end();
+  }
   console.log(myUrl);
   fs.appendFile("log.txt", log, (err, data) => {
     switch (myUrl.pathname) {
@@ -21,6 +23,8 @@ const server = http.createServer((req, res) => {
         break;
     }
   });
-});
+}
+
+const server = http.createServer(myHandler);
 
 server.listen(8000, () => console.log("Server Started"));
---
+++ Server1/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const express = require("express");;
+
+const app = express();
+
+app.get
+
+function myHandler(req, res){
+
+}
+
+const myServer = http.createServer(myHandler)
