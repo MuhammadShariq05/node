c7fb2acd64d1ee5b8ce41a0d696c2791638a6a22
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-04-22 11:06:36 +0530

    dynamic route learnt and get request done

--- Project-01/index.js
+++ Project-01/index.js
@@ -7,4 +7,14 @@ const PORT = 5000;
 app.get("/users", (req, res) => {
   return res.json(users);
 })
+
+// dynamic route ~ GET /api/users/:id 
+app.get("/users/:id",(req, res) =>{
+  // get id
+  const id = Number(req.params.id);
+  //now we have to find id in the json file.
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+})
+
 app.listen(PORT, () => {console.log(`Server started at Port ${PORT}`)});
