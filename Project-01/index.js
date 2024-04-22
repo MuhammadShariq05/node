a2213dd0caed157243bae7e3229d293302a440cb
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-04-22 11:23:28 +0530

    new way to write methods togethers when the routes are same

--- Project-01/index.js
+++ Project-01/index.js
@@ -6,15 +6,34 @@ const PORT = 5000;
 
 app.get("/users", (req, res) => {
   return res.json(users);
-})
+});
 
-// dynamic route ~ GET /api/users/:id 
-app.get("/users/:id",(req, res) =>{
+// dynamic route ~ GET /api/users/:id
+app.get("/users/:id", (req, res) => {
   // get id
   const id = Number(req.params.id);
   //now we have to find id in the json file.
   const user = users.find((user) => user.id === id);
   return res.json(user);
-})
+});
 
-app.listen(PORT, () => {console.log(`Server started at Port ${PORT}`)});
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    // get id
+    const id = Number(req.params.id);
+    //now we have to find id in the json file.
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {})
+  .delete((req, res) => {});
+
+// Post - create new user
+app.post("/users", (req, res) => {
+  
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started at Port ${PORT}`);
+});
