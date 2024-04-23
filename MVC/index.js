95526231dfd0e5a0147a939187b8979a73c8407f
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-04-23 20:25:06 +0530

    middleware

--- MVC/index.js
+++ MVC/index.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 // connection to mongodb
 connectMongo("mongodb://127.0.0.1:27017/project-MVC").then(() => {console.log("MongoDb connected")} )
 
-
+// middleware
 app.use(express.urlencoded({ extended: true }));
 
 // Route
