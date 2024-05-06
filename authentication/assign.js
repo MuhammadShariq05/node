1f31f409d530d65a779cc0634e3967dd377b6a3c
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-05-06 21:12:23 +0530

    done with decode and verify

--- authentication/assign.js
+++ authentication/assign.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const jwtPassword = "secret";
 const zod = require("zod");
 
+// using zod to verify the inpputs
 const emailSchema = zod.string().email();
 // password less then 6 wale category me hai
 const passwordSchema = zod.string().min(6);
@@ -23,5 +24,32 @@ function signJwt(username, password){
   return signature;
 }
 
-const ans = signJwt("shariqm", "shariq12342");
+// decode
+function decodeJwt(token){
+  const decode = jwt.decode(token);
+  if(decode){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+// verify
+function verifyJwt(token){
+  try{
+    jwt.verify(token, password);
+    return true
+  }catch(e){
+    console.log("error");
+  }
+  return false;
+}
+
+const ans = signJwt("shariqmello@gmail.com", "shariq12342");
 console.log(ans);
+
+const code = decodeJwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNoYXJpcW1lbGxvQGdtYWlsLmNvbSIsImlhdCI6MTcxNTAwOTU4NH0.aiMIKwMhT-ra7IbHMlznifzPYPYQO1ZYEBrDciIt3Fk");
+console.log(code);
+
+const ver = verifyJwt("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNoYXJpcW1lbGxvQGdtYWlsLmNvbSIsImlhdCI6MTcxNTAwOTU4NH0.aiMIKwMhT-ra7IbHMlznifzPYPYQO1ZYEBrDciIt3Fk");
+console.log(ver);
