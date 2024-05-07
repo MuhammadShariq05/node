9ce91288aa260da557864d3224e4ee6c83268bed
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-05-07 23:10:19 +0530

    working

--- Dom/class-4-harkirat/index.js
+++ Dom/class-4-harkirat/index.js
@@ -11,9 +11,18 @@ app.get("/sum", (req, res) => {
   res.send(sum.toString());
 });
 
-
+app.get("/interest", (req, res) => {
+  const principal = parseInt(req.query.principal);
+  const rate = parseInt(req.query.rate);
+  const time = parseInt(req.query.time);
+  const interest = (principal * rate * time) / 100;
+  const total = principal + interest;
+  res.send({
+    total: total,
+    interest: interest,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Sever started at port ${PORT}`);
 });
-
