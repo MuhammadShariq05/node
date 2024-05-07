f9b72db765ee84b7ec3e970a07d5909a806264e8
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-05-07 22:23:35 +0530

    connected backend to frontend

---
+++ Dom/class-4-harkirat/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const PORT = 5000;
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.get("/sum", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const sum = a + b;
+  res.send(sum.toString());
+});
+
+app.listen(PORT, ()=>{
+  console.log(`Sever started at port ${PORT}`);
+})
